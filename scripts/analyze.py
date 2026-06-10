66bb12b57203b0a54716f07ec3185165410b2c4b
John Le <jjuniorin@gmail.com>
2026-06-09 22:09:23 -0700

    feat(analyze_posts): run sentiment on all posts

--- scripts/analyze.py
+++ scripts/analyze.py
@@ -5,6 +5,7 @@ INPUT_FILE = "data/cleaned_posts.csv"
 OUTPUT_FILE = "data/results.csv"
 MODEL_NAME = "cardiffnlp/twitter-roberta-base-sentiment-latest"
 
+# Load twitter RoBERTa's model
 def load_model():
     try:
         model = pipeline("sentiment-analysis", model=MODEL_NAME)
@@ -13,6 +14,7 @@ def load_model():
     
     return model
 
+# Analyze and score text
 def analyze_sentiment(model, text):
     analyzed_results = model(text)
 
@@ -23,3 +25,13 @@ def analyze_sentiment(model, text):
         "label": label,
         "score": score,
     }
+
+# Analyze all posts and add to df
+def analyze_posts(model, df):
+    for i, text in enumerate(df["body_cleaned"]):
+        result = analyze_sentiment(model, text)
+        df.at[i, "sentiment"] = result["label"]
+        df.at[i, "score"] = result["score"]
+        if i % 10 == 0 and i > 0:
+            print(f"{i}/{len(df)} posts analyzed...")
+    return df
