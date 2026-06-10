769c53f0cea0cdf49ce29ffb43a5e4ee33379d4a
John Le <jjuniorin@gmail.com>
2026-06-10 00:10:36 -0700

    feat(get_top_keywords): return top n key words

--- scripts/keyword_analysis.py
+++ scripts/keyword_analysis.py
@@ -17,3 +17,16 @@ def extract_keywords(text):
     words = text_lower.split()
     words = [word for word in words if word not in STOP_WORDS]
     return words
+
+# Get top n key words from every post
+def get_top_keywords(df, n):
+    all_words = Counter()
+    for i, text in enumerate(df["body_cleaned"]):
+        extracted_keywords = extract_keywords(text)
+        all_words.update(extracted_keywords)
+        if i % 10 == 0 and i > 0:
+            print(f"{i}/{len(df)} posts counted...")
+
+    df = pd.DataFrame(all_words.most_common(n), columns=["word", "count"])
+
+    return df
